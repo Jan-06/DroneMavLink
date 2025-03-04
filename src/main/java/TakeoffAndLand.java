ee1a71800869181c5d2b2446a394148416e00b0d
jgerlach0906 <jangerlach2006@gmail.com>
2025-03-04 12:41:34 +0100

    v2

--- src/main/java/TakeoffAndLand.java
+++ src/main/java/TakeoffAndLand.java
@@ -1,40 +1,71 @@
-import io.mavsdk.System;
-import io.mavsdk.action.Action;
+import io.mavsdk.mission.Mission;
+import io.reactivex.CompletableSource;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
+import java.util.Scanner;
 
 
 public class TakeoffAndLand {
   private static final Logger logger = LogManager.getLogger();
 
   public static void main(String[] args) {
+    System.out.println("Test");
+    Scanner sc = new Scanner(System.in);
+
     logger.debug("Starting example: takeoff and land...");
 
-    System drone = new System();
+    io.mavsdk.System drone = new io.mavsdk.System();
     CountDownLatch latch = new CountDownLatch(1);
 
-    drone.getAction().arm()
-          .doOnComplete(() -> logger.debug("Arming..."))
-          .doOnError(throwable -> logger.error("Failed to arm: "
-                  + ((Action.ActionException) throwable).getMessage()))
-          .andThen(drone.getAction().takeoff()
-            .doOnComplete(() -> logger.debug("Taking off..."))
-            .doOnError(throwable -> logger.error("Failed to take off: "
-                    + ((Action.ActionException) throwable).getMessage())))
-          .delay(5, TimeUnit.SECONDS)
-          .andThen(drone.getAction().land()
-            .doOnComplete(() -> logger.debug("Landing..."))
-            .doOnError(throwable -> logger.error("Failed to land: "
-                    + ((Action.ActionException) throwable).getMessage())))
-          .subscribe(latch::countDown, throwable -> latch.countDown());
+    List<Mission.MissionItem> cycle = new ArrayList<>();
+    cycle.add(generateMissionItem(47.398039859999997, 8.5455725400000002));
+    cycle.add(generateMissionItem(47.398036222362471, 8.5450146439425509));
+    cycle.add(generateMissionItem(47.397825620791885, 8.5450092830163271));
+    cycle.add(generateMissionItem(47.397832880000003, 8.5455939999999995));
 
+    List<Mission.MissionItem> missionItems = new ArrayList<>();
+    for (int i = 0; i < 300; i++) {
+      missionItems.addAll(cycle);
+    }
+
+    Mission.MissionPlan missionPlan = new Mission.MissionPlan(missionItems);
+    logger.debug("About to upload " + missionItems.size() + " mission items");
+
+    drone.getMission()
+            .setReturnToLaunchAfterMission(true)
+            .andThen(drone.getMission().uploadMission(missionPlan)
+                    .doOnComplete(() -> logger.debug("Upload succeeded")))
+            .andThen(drone.getMission().downloadMission()
+                    .doOnSubscribe(disposable -> logger.debug("Downloading mission"))
+                    .doAfterSuccess(disposable -> logger.debug("Mission downloaded")))
+            .toCompletable()
+            .andThen((CompletableSource) cs -> latch.countDown())
+            .subscribe();
     try {
       latch.await();
     } catch (InterruptedException ignored) {
         // This is expected
     }
+  }
+
+  public static Mission.MissionItem generateMissionItem(double latitudeDeg, double longitudeDeg) {
+    return new Mission.MissionItem(
+            latitudeDeg,
+            longitudeDeg,
+            10f,
+            10f,
+            true,
+            0f,
+            0f,
+            Mission.MissionItem.CameraAction.NONE,
+            0f,
+            0d,
+            0f,
+            0f,
+            0f);
   }
 }
